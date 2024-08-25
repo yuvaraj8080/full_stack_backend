2b91a637938cbcdbc5a74fa1d435162bdf5335d8
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2024-08-25 11:46:03 +0530

    Initial commit

---
+++ Type_script/src/index.ts
@@ -0,0 +1,7 @@
+let data : string = "Sent me message";
+
+let num : number = 20
+
+let check  = false;
+console.log (data);
+
---
+++ Type_script/src/practice.ts
@@ -0,0 +1,15 @@
+function PrintData() : void{
+    console.log("MY Name is yuvaraj dekhane");
+}
+
+ function printAddition(num1: number, num2:number): number{
+    return num1 + num2;
+ }
+
+
+
+
+
+const ans: number = printAddition(4, 5);
+console.log(ans);
+PrintData();
